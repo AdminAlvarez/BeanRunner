d900b2a1266de7cade87e366a84d2a4ed75af6f3
FrancoJOG <03francojog1@gmail.com>
2026-09-23 17:21:53 -0600

    feat(qa): agregar Makefile inicial, main.cpp y script de verificacion para Hito 1 Closes #10

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "[BeanRunner] Servicio JobRunner inicializado correctamente.\n";
+    std::cout << "[BeanRunner] Entorno C++17 verificado sin advertencias de compilacion.\n";
+    return 0;
+}
